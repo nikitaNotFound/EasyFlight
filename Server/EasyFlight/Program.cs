db984ffad06fe99ddddcd760cc5bb78f7ce03bc5
keksualni <46090847+keksualni@users.noreply.github.com>
2019-07-16 11:38:08 +0300

    Structure fix

--- Server/EasyFlight/Program.cs
+++ Server/EasyFlight/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Easy_Flight
+namespace EasyFlight
 {
     public class Program
     {
