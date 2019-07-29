77eebf67622580e116016694d50923370ad10b2f
keksualni <niptungo@gmail.com>
2019-07-29 16:32:09 +0300

    bug fix

--- Frontend/src/components/admin/tabs/airports/edit.js
+++ Frontend/src/components/admin/tabs/airports/edit.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import Headline from './headline';
 import PropsTypes from 'prop-types';
-import BuyIcon from '../../../../icons/add-image.png';
 
 class Edit extends Component {
     static propsTypes = {
         name: PropsTypes.string,
         city: PropsTypes.string,
         country: PropsTypes.string,
-        desc: PropsTypes.string
+        desc: PropsTypes.string,
+        img: PropsTypes.string
     }
 
     render () {
@@ -21,19 +21,19 @@ class Edit extends Component {
                         <div className="col-2">
                             <input type="file" name="image" id="file-input" className="file-upload"/>
                             <label htmlFor="file-input">
-                                <img src={BuyIcon} className="adding-form-img"/>
+                                <img src={this.props.img} className="adding-form-img"/>
                             </label>
                         </div>
                         <div className="col-10">
                             <div className="form-item">
-                                <input type="text" placeholder="airport name" name="name"/>
+                                <input type="text" name="name" value={this.props.name}/>
                             </div>
                             <div className="form-item">
-                                <input type="text" placeholder="city" name="city"/>
-                                <input type="text" placeholder="country" name="country"/>
+                                <input type="text" name="city" value={this.props.city}/>
+                                <input type="text" name="country" value={this.props.country}/>
                             </div>
                             <br/>
-                            <textarea placeholder="description"/>
+                            <textarea value={this.props.desc}/>
                         </div>
                     </div>
                             <input type="submit" value="Save" className="add-button"/>
