77e00c700382c443cc8860ce6c8636bd1813c868
keksualni <niptungo@gmail.com>
2019-08-02 13:51:56 +0300

    Update AirplanesService.js

--- Frontend/src/services/AirplanesService.js
+++ Frontend/src/services/AirplanesService.js
@@ -6,12 +6,32 @@ export function getAll () {
     }
 
     return [
-        {name:"F300", rows:2, maxMass: 1000, sitsInfo:[
-            [sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass],
-            [sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass, sitsTypes.economClass]
+        {name:"F300", maxMass: 1000, sits:[
+            new Sit(1, 1, 1, 1, sitsTypes.economClass), new Sit(1, 1, 1, 2, sitsTypes.economClass),
+            new Sit(1, 1, 2, 1, sitsTypes.economClass), new Sit(1, 1, 2, 2, sitsTypes.economClass),
+            new Sit(1, 2, 1, 1, sitsTypes.businesClass), new Sit(1, 2, 1, 2, sitsTypes.businesClass),
+            new Sit(1, 2, 2, 1, sitsTypes.businesClass), new Sit(1, 2, 2, 2, sitsTypes.businesClass)
         ]},
-        {name:"Keksik", rows:1, maxMass: 500, sitsInfo:[
-            [sitsTypes.firstClass, sitsTypes.firstClass, sitsTypes.firstClass, sitsTypes.businesClass]
+        {name:"Keksik", maxMass: 500, sits:[
+            new Sit(1, 1, 1, 1, sitsTypes.economClass), new Sit(1, 1, 1, 2, sitsTypes.economClass)
         ]}
     ];
 }
+
+class Sit {
+    floor;
+    section;
+    row;
+    number;
+    type;
+
+    constructor(floor, section, row, number, type) {
+        this.floor = floor;
+        this.section = section;
+        this.row = row;
+        this.number = number;
+        this.type = type;
+    }
+}
+
+export default Sit;
