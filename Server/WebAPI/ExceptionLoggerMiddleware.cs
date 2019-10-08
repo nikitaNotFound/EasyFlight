8e541cd0fd01f92143cc69abf33be1dceb8dd133
keksualni <niptungo@gmail.com>
2019-10-08 16:01:15 +0300

    main update

---
+++ Server/WebAPI/ExceptionLoggerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebAPI
+{
+    public static class ExceptionLoggerExtensions
+    {
+        public static IApplicationBuilder UseExceptionLogger(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionLoggerMiddleware>();
+        }
+    }
+
+    public class ExceptionLoggerMiddleware
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        private RequestDelegate _next;
+
+
+        public ExceptionLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message);
+            }
+        }
+    }
+}
