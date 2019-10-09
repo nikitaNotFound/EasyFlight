f2f218a57891352d590fa1d936f5b644dfcf74ce
keksualni <niptungo@gmail.com>
2019-10-09 17:42:17 +0300

    structure setup

---
+++ Server/DataAccessLayer/Repositories/Users/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories.Users
+{
+    public interface IUserRepository
+    {
+    }
+}
---
+++ Server/DataAccessLayer/Repositories/Users/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories.Users
+{
+    class UserRepository
+    {
+        private IDalSettings _settings;
+
+    }
+}
