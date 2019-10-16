2c1bf02211230b8d004123455bd3ba528f513e3d
keksualni <niptungo@gmail.com>
2019-10-16 15:57:33 +0300

    database update

---
+++ Server/Database/dbo/Stored Procedures/addAirport.sql
@@ -0,0 +1,8 @@
+create procedure [dbo].[AddAirport]
+    @name as nvarchar(50),
+    @cityId as int
+as
+    insert into Airports (Name, CityId)
+    values (@name, @cityId)
+
+    select SCOPE_IDENTITY();
