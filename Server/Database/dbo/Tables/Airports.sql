c23252e5f153f333330f1c7b250504e720f6ced6
keksualni <niptungo@gmail.com>
2019-10-17 16:42:51 +0300

    database update

--- Server/Database/dbo/Tables/Airports.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Airports] (
-    [Id]     INT           IDENTITY (1, 1) NOT NULL,
-    [Name]   NVARCHAR (50) NOT NULL,
-    [CityId] INT           NOT NULL,
-    CONSTRAINT [PK_Airports] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
