df31d9eeb46fc1233f94bdb3d4faf6161e41c03c
keksualni <niptungo@gmail.com>
2019-10-17 16:52:59 +0300

    format fix

--- Server/Database/dbo/Stored Procedures/AddCountry.sql
+++ Server/Database/dbo/Stored Procedures/AddCountry.sql
@@ -1,6 +1,6 @@
 ﻿create procedure [dbo].[AddCountry]
     @name as nvarchar(60)
 as
-	insert into Countries (Name)
-	values (@name)
-	select SCOPE_IDENTITY();
+    insert into Countries (Name)
+    values (@name)
+    select SCOPE_IDENTITY();
