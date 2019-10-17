729e306c956418a613b4c0811aaff89db1f43ef1
keksualni <niptungo@gmail.com>
2019-10-17 19:02:21 +0300

    structure setup

---
+++ Server/WebAPI/Controllers/AirplanesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableCors("CorsPolicy")]
+    public class AirplanesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+
+        public AirplanesController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+
+        // GET api/airplanes{?nameFilter}
+        [HttpGet]
+        public async Task<ActionResult> GetAsync(string nameFilter)
+        {
+            return Ok();
+        }
+
+        // GET api/airplanes/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> GetByIdAsync(int id)
+        {
+            return Ok();
+        }
+
+        // GET api/airplanes/{airplaneId}/seats
+        [HttpGet]
+        [Route("{airplaneId}/seats")]
+        public async Task<ActionResult> GetAirplaneSeatsAsync(int airplaneId)
+        {
+            return Ok();
+        }
+
+        // GET api/airplanes/{airplaneId}/seat-types
+        [HttpGet]
+        [Route("{airplaneId}/seat-types")]
+        public async Task<ActionResult> GetAirplaneSeatTypesAsync(int airplaneId)
+        {
+            return Ok();
+        }
+
+        // POST api/airplanes
+        [HttpPost]
+        public async Task<ActionResult> AddAirplaneAsync([FromBody] Airplane airplane)
+        {
+            return Ok();
+        }
+    }
+}
