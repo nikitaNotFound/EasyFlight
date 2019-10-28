5b625e86a49b5e20cc8c3e4396492f958b0b58c3
keksualni <niptungo@gmail.com>
2019-10-28 17:08:18 +0300

    JWT-authentification Main page became AuthorizedRoute, Removing logging package from BusinessLayer, naming fixes in sql tables, ReciveAccount model (WebAPI assembly) renamed into the RequestAccount

---
+++ Server/Database/dbo/Tables/Cities.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Cities] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (50) NOT NULL,
+    [CountryId] INT           NOT NULL,
+    CONSTRAINT [PK_Cities] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
