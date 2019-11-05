e61147d542368d28945d1ad8a37dc607f6132281
keksualni <niptungo@gmail.com>
2019-11-05 18:58:14 +0300

    flights-api FlightService description

---
+++ Server/BusinessLayer/Models/FlightCost.cs
@@ -0,0 +1,9 @@
+namespace BusinessLayer.Models
+{
+    public class FlightCost
+    {
+        public int FlightId { get; set; }
+        public int SeatTypeId { get; set; }
+        public int Cost { get; set; }
+    }
+}
