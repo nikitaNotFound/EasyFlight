1bbf7a58caeb771d0220584f52651dc87fc5e951
keksualni <niptungo@gmail.com>
2019-11-05 19:07:45 +0300

    flights-api FlightRepository description

---
+++ Server/DataAccessLayer/Models/FlightCostEntity.cs
@@ -0,0 +1,9 @@
+namespace DataAccessLayer.Models
+{
+    public class FlightCostEntity
+    {
+        public int FlightId { get; set; }
+        public int SeatTypeId { get; set; }
+        public int Cost { get; set; }
+    }
+}
