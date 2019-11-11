e61ceaf8d46f37d159b60712dedb2902a3a0ab84
keksualni <keksualni@users.noreply.github.com>
2019-11-11 14:29:43 +0300

    booking-api database scripts changes (added book type to FlightSeatsInfo)

--- Server/Database/dbo/Tables/FlightSeatsInfo.sql
+++ Server/Database/dbo/Tables/FlightSeatsInfo.sql
@@ -1,7 +1,7 @@
-﻿create table [dbo].[FlightSeatsInfo] (
-    [FlightId]   int                not null,
-    [AccountId]  int                not null,
-    [SeatId]     int                not null,
-    [AirplaneId] int                not null,
-    [BookTime]   datetimeoffset (7) not null
-);
+﻿create table [dbo].[FlightSeatsInfo](
+    [FlightId] [int] NOT NULL,
+    [AccountId] [int] NOT NULL,
+    [SeatId] [int] NOT NULL,
+    [BookTime] [datetimeoffset](7) NOT NULL,
+    [BookType] [int] NOT NULL
+)
